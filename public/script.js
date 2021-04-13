b808363344763dc6103414a3a02d297108fe1002
MakeItMike <mbwoodyard@hotmail.com>
2021-04-13 00:13:52 -0400

    phone

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ let socket = io();
 // Button.
 function submitClick() {
     // Sends the website entered from the input to server.
-    socket.emit("website", document.getElementById('email').value);
+    socket.emit("website", document.getElementById('website-url').value);
 }
 
 // Receives the email.
@@ -17,4 +17,16 @@ function receiveEmail(emails) {
     console.log(emails);
 }
 
+// Receives the phone numbers.
+function receivePhone(phones) {
+        // Displays the email client-side.
+    for(let i = 0; i < phones.length; i++){
+        let phone = document.createElement("p");
+        phone.innerText = phones[i];
+        document.getElementById('phone-display').appendChild(phone);
+    }
+    console.log(phones);
+}
+
 socket.on("emails", receiveEmail);
+socket.on("phone", recievePhone);
